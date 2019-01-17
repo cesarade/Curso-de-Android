7ae1733d1c20e42a42435c15a287a20b14bc0015
cesaralcivar <cesar_ade@hotmail.com>
2019-01-17 16:01:38 -0500

    Creando mi primera aplicación en Android

--- AndroidProjects/GeoQuiz/app/src/main/java/com/cursoandroid/geoquiz/QuizActivity.java
+++ AndroidProjects/GeoQuiz/app/src/main/java/com/cursoandroid/geoquiz/QuizActivity.java
@@ -2,12 +2,38 @@ package com.cursoandroid.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button verdaderoButton;
+    private Button falsoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        verdaderoButton = findViewById(R.id.verdadero_button);
+        verdaderoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                Toast.makeText(QuizActivity.this,
+                        R.string.correcto_toast,
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        falsoButton = findViewById(R.id.falso_button);
+        falsoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                Toast.makeText(QuizActivity.this,
+                        R.string.incorrecto_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
